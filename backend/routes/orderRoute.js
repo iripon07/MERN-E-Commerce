ae71955b9cfcd0f727cea9fb11985537e15efeff
AsadulIslamRipon <mdasadulripon@gmail.com>
2023-08-09 13:16:00 +0600

    Order completed

--- backend/routes/orderRoute.js
+++ backend/routes/orderRoute.js
@@ -1,9 +1,32 @@
 const express = require('express')
 const router = express.Router()
-const { isAuthenticatedUser, authorizedRoles } = require('../middleware/auth');
-const { newOrder } = require('../controllers/orderController');
+const { 
 
-router.route("/order/new").post(isAuthenticatedUser, newOrder)
+    isAuthenticatedUser, 
+    authorizedRoles 
 
+} = require('../middleware/auth');
+const { 
+
+    newOrder, 
+    myOrders, 
+    getSingleOrder, 
+    etAllOrders, 
+    updateOrder, 
+    deleteOrder 
+
+} = require('../controllers/orderController');
+
+router.route("/order/new").post(isAuthenticatedUser, newOrder);
+
+router.route("/order/:id").get(isAuthenticatedUser, authorizedRoles("admin"), getSingleOrder);
+
+router.route("/orders/me").get(isAuthenticatedUser, myOrders);
+
+router.route("/admin/orders").get(isAuthenticatedUser, authorizedRoles("admin"), getAllOrders);
+
+router.route("/admin/order/:id")
+.put(isAuthenticatedUser, authorizedRoles("admin"), updateOrder)
+.delete(isAuthenticatedUser, authorizedRoles("admin"), deleteOrder);
 
 module.exports = router
